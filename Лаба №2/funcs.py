c414a712bbe83ab777297d8622164c60312d9d55
daniilsan347 <daniilsan347@gmail.com>
2021-10-26 22:33:50 +0300

    добавлены все функции

--- Лаба №2/funcs.py
+++ Лаба №2/funcs.py
@@ -1,5 +1,6 @@
-# Laba No2
+# Laba No2 - Functions module
 
+import random
 from typing import *
 
 quick_sort = lambda list: sorted(list)
@@ -17,9 +18,85 @@ def find_el(list: List, element: Any):
     return False
 
 def find_few_els(list: List, elements_list: List):
+  """
+  Returns list of tuples with index and item. If there is no searched item, it won't apear in list.
+  """
   result = []
   for el in elements_list:
     found_index = find_el(list, el)
     if found_index != False:
       result.append(tuple([found_index, el]))
   return result.sort(key = lambda item: item[0])
+
+def find_5_min_nums(list: List):
+  """
+  Returns list of 5 minimum numbers. If there is string in list, returns False
+  """
+  list_float = []
+  result = []
+  try:
+    for el in list:
+      list_float.append(float(el))
+  except ValueError as err:
+    print("List contains strings.", err)
+    return False
+  for el in range(5):
+    min_num = min(list_float)
+    result.append(min_num)
+    list_float.remove(min_num)
+  return result
+
+def find_5_max_nums(list: List):
+  """
+  Returns list of 5 maximum numbers. If there is string in list, returns False
+  """
+  list_float = []
+  result = []
+  try:
+    for el in list:
+      list_float.append(float(el))
+  except ValueError as err:
+    print("List contains strings.", err)
+    return False
+  for el in range(5):
+    min_num = max(list_float)
+    result.append(min_num)
+    list_float.remove(min_num)
+  return result
+
+def average(list: List):
+  """
+  Returns average float number of all numbers in list
+  """
+  list_float = []
+  try:
+    for el in list:
+      list_float.append(float(el))
+  except ValueError as err:
+    print("List contains strings.", err)
+  result = sum(list_float)/len(list_float)
+  return result
+
+def remove_duplicates(list: List):
+  """
+  Returns list without duplicates
+  """
+  sorted_list = quick_sort(list)
+  for i in range(len(sorted_list)-1):
+    if sorted_list[i] == sorted_list[i+1]:
+      sorted_list.pop(i)
+  return sorted_list
+
+def pprint_list(list: List):
+  print("[ ", end="")
+  for i in range(len(list)-1):
+    print(list[i], end=", ")
+  print(list[-1], "]")
+
+def gen_randint_list(min, max, count):
+  """Generates list of random integers"""
+  return [random.randint(min, max) for i in range(count)]
+
+def gen_randfloat_list(count):
+  """Generates list of random floats"""
+  return [random.random()*100 for i in range(count)]
